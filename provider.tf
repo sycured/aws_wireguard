43c965f768e3341e123705a45d340ec42bf231b4
sycured <60801403+sycured@users.noreply.github.com>
2021-02-20 18:59:58 -0500

    [provider] remove version: deprecated

--- provider.tf
+++ provider.tf
@@ -1,5 +1,4 @@
 provider "aws" {
-  version                 = "> 2.69"
   region                  = var.region
   shared_credentials_file = var.shared_credentials_file
   profile                 = var.aws_profile
